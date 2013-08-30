bb50f1cef92a6bc1d67cb99c9335f1e362a3cc85
lis <lis.ebuy@gmail.com>
2013-08-30 14:38:37 +0000

    adjust layout

--- web.js
+++ web.js
@@ -4,8 +4,17 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var getFileName=function(request){
+    var path=__dirname+request.path;
+    return path;
+};
 app.get('/', function(request, response) {
   response.send(content);
+});
+
+app.get('/*.png',function(request,response){
+    var f=getFileName(request);
+    response.sendfile(f);
 });
 
 var port = process.env.PORT || 8080;
